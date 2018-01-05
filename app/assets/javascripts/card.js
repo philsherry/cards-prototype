78abd45a36f624061ac4ffdbb60b7f2db504b325
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2018-01-05 14:51:45 +0000

    PTA card pattern.

---
+++ app/assets/javascripts/card.js
@@ -0,0 +1,32 @@
+//Every time a user mouse enters a card, return that card
+
+//Now we want to:
+//Change the background-color
+//Change the font-color
+
+var card = (function () {
+
+  //Capture when a user clicks
+  $('.card-link').on('click', function() {
+    var url = $(this).find('a').attr('href')
+    window.location.href = url
+  })
+
+  //Find the max-height for all cards
+  //Get collection of all cards
+  function setMaxheight(ele){
+    var height = []
+
+    $(ele).each(function(){
+      height.push($(this).height())
+    })
+
+    var maxHeight = height.sort(function(a, b){return b-a})[0]
+    $(ele).height(maxHeight)
+  }
+
+  setMaxheight('.pta-card')
+  setMaxheight('.card-link')
+
+
+})()
